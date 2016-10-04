f11969a19fcd7d8de8887b85a3340555668cae89
BenjaminUbuntu <bjgolard@gmail.com>
2016-10-04 15:39:41 +0200

    a

--- src/Certificat.java
+++ src/Certificat.java
@@ -65,6 +65,56 @@ public class Certificat {
 			e.printStackTrace();
 		}
 	}
+	Certificat(String issuer, String subject, PublicKey clePub, PrivateKey signature, int validityDays)
+	{
+		// On recupere la cle publique et la cle privee :
+				PublicKey pubkey = clePub;
+				PrivateKey privkey = signature;
+				// On cree la structure qui va nous permettre de creer le certificat
+				X509V1CertificateGenerator certGen = new X509V1CertificateGenerator();
+				// Le certificat sera valide pour 10 jours
+				Calendar expiry = Calendar.getInstance();
+				Date startDate = expiry.getTime();
+				expiry.add(Calendar.DAY_OF_YEAR, validityDays);
+				Date expiryDate = expiry.getTime();
+				certGen.setNotBefore(startDate);
+				certGen.setNotAfter(expiryDate);
+				// On le positionne dans le futur certificat
+				seqnum=seqnum.add(BigInteger.ONE);
+				certGen.setSerialNumber(seqnum);
+				// Le nom du proprietaire et du certificateur :
+				// ici, les memes car auto-signe.
+				X500Principal cnNameSubject = new X500Principal("CN="+subject);
+				X500Principal cnNameIssuer = new X500Principal("CN="+issuer);
+				certGen.setSubjectDN(cnNameSubject);
+				certGen.setIssuerDN(cnNameIssuer);
+				// L'algorithme de signature utilise pour la certification
+				certGen.setSignatureAlgorithm("sha1WithRSA");
+				// La cle a certifier
+				certGen.setPublicKey(pubkey);
+				// On calcule le certificat au format X509 !
+				try {
+					this.x509 = certGen.generate(privkey, "BC");
+				} catch (CertificateEncodingException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (InvalidKeyException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IllegalStateException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchProviderException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (NoSuchAlgorithmException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (SignatureException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+	}
 	
 	public boolean verifCertif (PublicKey pubkey) 
 	{
--- src/Equipement.java
+++ src/Equipement.java
@@ -1,5 +1,6 @@
 import java.security.*;
 import java.util.HashMap;
+import java.util.Map;
 
 public class Equipement 
 {
@@ -7,7 +8,7 @@ public class Equipement
 	private Certificat monCert; // Le certificat auto-signe.
 	private String monNom; // Identite de l’equipement.
 	private int monPort; // Le numéro de port d’ecoute.
-	private HashMap<String, PublicKey> ca; 
+	private HashMap<Equipement,Certificat> ca; 
 	private HashMap<String, PublicKey> da;
 	
 	Equipement (String nom, int port) throws Exception 
@@ -20,7 +21,7 @@ public class Equipement
 		this.monCert=new Certificat(this.monNom, this.maCle, 10);
 		this.autoVerif();
 		this.affichage();
-		this.ca=new HashMap<String, PublicKey>();
+		this.ca=new HashMap<Equipement,Certificat>();
 		this.da=new HashMap<String, PublicKey>();
 	}
 	
@@ -73,4 +74,26 @@ public class Equipement
 			System.out.println("Certificat autosigné non vérifié.");
 		}
 	}
+	private void certification(Equipement other)
+	{
+		// other possède un certificat de this portant sur la clé publique de other
+		Certificat certif = new Certificat(this.monNom(), other.monNom(), other.maClePub(), this.maCle.Privee(), 10);
+		// on vérifie
+		if (certif.verifCertif(this.maClePub())) // si le certificat est vérifié, on ajoute this au CA de other
+		{
+			other.ajoutCA(this, certif);
+			System.out.println("Le certificat est valide.");
+		}
+		else
+		{
+			System.out.println("Le certificat n'est pas valide.");
+		}
+	}
+	public void ajoutCA(Equipement other, Certificat certif)
+	{
+		// ajoute dans le CA de this, other
+		this.ca.put(other, certif);
+	}
+
+	
 }
